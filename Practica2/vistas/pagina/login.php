f4fac5203e95d2bc8c74c076367ba171a1b08b67
cviverob <cvivero@ucm.es>
2024-03-01 15:07:25 +0100

    Cambio de estructura

---
+++ Practica2/vistas/pagina/login.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <html lang "es">
+    <header>
+        <title>Elmo Cines</title>
+        <meta charset = "utf-8">
+    </header>
+    <body>
+        <?php require("../comun/cabecera.php") ?>
+    </body>
+</html>
