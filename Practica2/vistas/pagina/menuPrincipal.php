784eb8379b8d16a51586e45853e6960460f3daca
OscarLopezCentenera <oscarl03@ucm.es>
2024-03-06 17:55:33 +0100

    Cartelera

---
+++ Practica2/vistas/pagina/menuPrincipal.php
@@ -0,0 +1,13 @@
+<?php 
+
+    $rutaEstilos = '../comun/estilo.css'
+    $tituloPagina = 'Cartelera';
+
+    $contenidoPrincipal = <<<EOS
+        <img src = "../../img/Dune.png">
+
+        <a href = "consultaPelicula.php"><button type = "button">Ir a la peli</button>
+    EOS;
+
+    require '../comun/plantilla.php';
+?>
