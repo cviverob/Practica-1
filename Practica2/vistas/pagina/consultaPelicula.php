dd5d6f4c265ff972cf4c5b2a58d987eb0f028d92
cviverob <cvivero@ucm.es>
2024-03-06 17:54:24 +0100

    Uso de EOS

--- Practica2/vistas/pagina/consultaPelicula.php
+++ Practica2/vistas/pagina/consultaPelicula.php
@@ -1,43 +1,42 @@
-<!DOCTYPE html>
-<html lang "es">
-    <header>
-        <meta charset="utf8mb4">
-		<link rel="stylesheet" type="text/css" href="../comun/estilo.css" />
-        <title>Elmo cines</title>
-    </header>
-    <body>
-        <?php 
-            require("../comun/cabecera.php");
-            require_once("../../src/peliculas/peliculas.php");
-        ?>
-        <main>
-            <?php
-                $pelicula = Pelicula::crea("Alejandría", "mucho texto jajaja", "../../img/Logo.png", "", 18, "acción", 120);
-                echo "<h1>" . $pelicula->getTitulo() . "</h1>";
-                // Faltan las sesiones
-                echo "<img src = \"" . $pelicula->getRutaPoster() . "\" alt = \"Póster de la película\">";
-                echo "<p> Sinopsis: " . $pelicula->getSinopsis() . "</p>";
-                echo "<p> Edad mínima: " . $pelicula->getPegi() . "</p>";
-                echo "<p> Género: " . $pelicula->getGenero() . "</p>";
-                echo "<p> Género: " . $pelicula->getDuracion() . " minutos </p>";
-                /*
-                if (!isset($_SESSION["pelicula"]) || !isset($_SESSION["pelicula"])) {
-                    echo "<h1>Error:</h1>";
-                    echo "<p>Película no encontrada</p>";
-                }
-                else {
-                    $pelicula = $_SESSION["pelicula"];
-                    echo "<h1>" . $pelicula->getTitulo() . "</h1>";
-                    // Faltan las sesiones
-                    echo "<img src = \"" . $pelicula->getRutaPoster() . "\" alt = \"Póster de la película\">";
-                    echo "<p> Sinopsis: " . $pelicula->getSinopsis() . "</p>";
-                    echo "<p> Edad mínima: " . $pelicula->getPegi() . "</p>";
-                    echo "<p> Género: " . $pelicula->getGenero() . "</p>";
-                    echo "<p> Género: " . $pelicula->getDuracion() . "minutos </p>";
-                    // Falta el trailer"
-                }*/
-            ?>
-        </main>
-        <?php require("../comun/pie.php") ?>
-    </body>
-</html>
+<?php
+require_once('../../includes/config.php');
+require_once('../../src/peliculas/peliculas.php');
+
+$rutaEstilos = '../comun/estilo.css';
+
+$tituloPagina = 'Proceso de login';
+
+// Película de ejemplo
+$pelicula = Pelicula::crea("Dune", "El duque Paul Atreides se une a los Fremen y comienza un 
+viaje espiritual y marcial para convertirse en Muad'dib, mientras intenta evitar el horrible 
+pero inevitable futuro que ha presenciado: una Guerra Santa en su nombre, que se extiende por
+ todo el universo conocido. Secuela de la película estrenada en 2021.", "../../img/Dune.png", 
+ "", 18, "acción", 120);
+
+// Faltan las sesiones y el trailer
+/*
+    if (!isset($_SESSION["pelicula"]) || !isset($_SESSION["pelicula"])) {
+        echo "<h1>Error:</h1>";
+        echo "<p>Película no encontrada</p>";
+    }
+    else {
+        $pelicula = $_SESSION["pelicula"];
+        echo "<h1>" . $pelicula->getTitulo() . "</h1>";
+        // Faltan las sesiones
+        echo "<img src = \"" . $pelicula->getRutaPoster() . "\" alt = \"Póster de la película\">";
+        echo "<p> Sinopsis: " . $pelicula->getSinopsis() . "</p>";
+        echo "<p> Edad mínima: " . $pelicula->getPegi() . "</p>";
+        echo "<p> Género: " . $pelicula->getGenero() . "</p>";
+        echo "<p> Género: " . $pelicula->getDuracion() . "minutos </p>";
+    } 
+*/
+$contenidoPrincipal = <<< EOS
+    <h1>{$pelicula->getTitulo()}</h1>
+    <img src = "{$pelicula->getRutaPoster()}" alt = \"Póster de la película\">";
+    <p> Sinopsis: {$pelicula->getSinopsis()} </p>
+    <p> Edad mínima: {$pelicula->getPegi()} </p>
+    <p> Género:  {$pelicula->getGenero()} </p>
+    <p> Género: {$pelicula->getDuracion()} minutos </p>
+EOS;
+
+require('../comun/plantilla.php');
