b159f6f19d6fb921da33863119a4a908af71de1a
OscarLopezCentenera <oscarl03@ucm.es>
2024-03-06 17:33:26 +0100

    Mini modificación

--- Practica2/vistas/pagina/seleccionDeButacas.php
+++ Practica2/vistas/pagina/seleccionDeButacas.php
@@ -1,16 +1,11 @@
-<!DOCTYPE html>
-<html lang "es">
-    <header>
-        <meta charset="utf8mb4">
-		<link rel="stylesheet" type="text/css" href="../comun/estilo.css" />
-        <title>Elmo cines</title>
-    </header>
-    <body>
-        <?php require("../comun/cabecera.php") ?>
-        <main>
-            <h1>Selección de butacas<h1>
-            <a href = "procesarCompra.php"><button type = "button">Comprar</button></a>
-        </main>
-        <?php require("../comun/pie.php") ?>
-    </body>
-</html>
+<?php
+
+    $rutaEstilos = '../comun/estilo.css';
+    $tituloPagina = 'Seleccion de Butacas';
+
+    $contenidoPrincipal = <<< EOS 
+        <h1>Selección de butacas<h1>
+        <a href = "procesarCompra.php"><button type = "button">Comprar</button></a>
+    EOS;
+
+?>
