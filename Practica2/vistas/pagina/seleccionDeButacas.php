673b4fd704f430d19876a02b17f45dbf0080404f
cviverob <cvivero@ucm.es>
2024-03-07 09:27:42 +0100

    Terminado

--- Practica2/vistas/pagina/seleccionDeButacas.php
+++ Practica2/vistas/pagina/seleccionDeButacas.php
@@ -1,11 +1,16 @@
 <?php
-
+    require_once('../../includes/config.php');
+    
     $rutaEstilos = '../comun/estilo.css';
+    
     $tituloPagina = 'Seleccion de Butacas';
-
-    $contenidoPrincipal = <<< EOS 
+    
+    // Ver por qué no funciona
+    // $correo = isset($_SESSION["correo"]) ?? "";
+    
+    $contenidoPrincipal = <<< EOS
         <h1>Selección de butacas<h1>
         <a href = "procesarCompra.php"><button type = "button">Comprar</button></a>
     EOS;
-
-?>
+    
+    require('../comun/plantilla.php');
