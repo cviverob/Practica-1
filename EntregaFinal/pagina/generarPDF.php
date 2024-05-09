f2add1ed63f0457c113d86d5efc2468b47289a65
LucasBarriosMartin <lucasb02@ucm.es>
2024-05-09 23:28:07 +0200

    cambios-css

--- EntregaFinal/pagina/generarPDF.php
+++ EntregaFinal/pagina/generarPDF.php
@@ -1,55 +1,59 @@
 <?php
-    require_once('../includes/config.php');
-        
-    $idCompra = $_GET['idCompra'];
-    $idUsuario = $_SESSION['id'];
-    $compra = es\ucm\fdi\aw\compra::buscarPorIdYUsuario($idCompra, $idUsuario);
-    $sesion = es\ucm\fdi\aw\sesion::buscar($compra->getIdSesion());
-    $pelicula = es\ucm\fdi\aw\pelicula::buscarPorNombre($compra->getTituloPeli());
-    $sala = es\ucm\fdi\aw\salas::buscar($sesion->getIdSala());
-    $rutaPoster = RUTA_APP . RUTA_PSTR . '/' . $pelicula->getRutaPoster();
-    $tituloPagina = 'ImprimirTicket';
-    $contenidoPrincipal = <<< EOS
-            <div class = 'superiorTicket'>
-                <div class = 'imagenTicket'>
-                    <img src = $rutaPoster>
-                </div>
-                <div class = 'descripcionTicket'>
-                    <h2>{$compra->getTituloPeli()}</h2>
-                    <p>{$pelicula->getSinopsis()}</p>
-                </div>
-            </div>
-        EOS;
-
-        $butacas = $compra->getButacas();
-        $contador = 1;
-        foreach($butacas as $b){
-            $partes = explode("-", $b);
-            $contenidoPrincipal .= <<< EOS
-                    <div class = 'infoTicket'>
-                        <h1>ENTRADA $contador</h1>
-                        <p>Gracias por comprar una entrada. La sala de la película es la {$sala->getNumSala()}. 
-                        No olvides que la película empezará a las {$sesion->getHoraIni()} y finalizará a las {$sesion->getHoraFin()} 
-                        y que tendrás que asistir el dia {$sesion->getFecha()} 10 minutos antes para poder sentarte en tu asiento. 
-                        Por si no te acuerdas, tu asiento está en la fila $partes[0] y la columna $partes[1].
-                        </p>
-                        <p>Fecha de compra: {$compra->getFecha()} {$compra->getHora()}</p>
-                    
-                    </div>
-                EOS;
-            $contador++;
-        }
-
-
-
-    $contenidoPrincipal .= <<< EOS
-    <button onclick="imprimirTicket()">Imprimir Ticket</button>
-    EOS;
-    require_once(RUTA_RAIZ . RUTA_PLNT);  
-    ?>
-    <script>
-        function imprimirTicket() {
-            window.print();
-        }
-    </script>
+require_once('../includes/config.php');
     
+$idCompra = $_GET['idCompra'];
+$idUsuario = $_SESSION['id'];
+$compra = es\ucm\fdi\aw\compra::buscarPorIdYUsuario($idCompra, $idUsuario);
+$sesion = es\ucm\fdi\aw\sesion::buscar($compra->getIdSesion());
+$pelicula = es\ucm\fdi\aw\pelicula::buscarPorNombre($compra->getTituloPeli());
+$sala = es\ucm\fdi\aw\salas::buscar($sesion->getIdSala());
+$rutaPoster = RUTA_APP . RUTA_PSTR . '/' . $pelicula->getRutaPoster();
+$tituloPagina = 'ImprimirTicket';
+$contenidoPrincipal = <<< EOS
+<div id="Ticket">
+    <div class="Ticket-columna">
+                <img src="$rutaPoster" class = "fotitoTicket">
+    </div>
+    <div class="Ticket-columna">
+            <h2>{$compra->getTituloPeli()}</h2>
+            <p>{$pelicula->getSinopsis()}</p>
+    </div>
+</div>
+EOS;
+
+
+$butacas = $compra->getButacas();
+$contador = 1;
+foreach ($butacas as $b) {
+    $partes = explode("-", $b);
+    $contenidoPrincipal .= <<< EOS
+        <div class="infoTicket">
+            <div class="entrada">
+                <h1>ENTRADA $contador</h1>
+            </div>
+            <div class="linea-vertical"></div> <!-- Línea vertical -->
+            <div class="descripcion">
+                <p>Gracias por comprar una entrada. La sala de la película es la {$sala->getNumSala()}.
+                No olvides que la película empezará a las {$sesion->getHoraIni()} y finalizará a las {$sesion->getHoraFin()}
+                y que tendrás que asistir el día {$sesion->getFecha()} 10 minutos antes para poder sentarte en tu asiento.
+                Por si no te acuerdas, tu asiento está en la fila $partes[0] y la columna $partes[1].
+                </p>
+                <p class="subliminal">Fecha de compra: {$compra->getFecha()} {$compra->getHora()}</p>
+            </div>
+        </div>
+    EOS;
+    $contador++;
+}
+
+
+$contenidoPrincipal .= <<< EOS
+<a href="#" onclick="imprimirTicket()" class="seleccionarPelicula">Imprimir Ticket</a>
+</div>
+EOS;
+require_once(RUTA_RAIZ . RUTA_PLNT);  
+?>
+<script>
+    function imprimirTicket() {
+        window.print();
+    }
+</script>
